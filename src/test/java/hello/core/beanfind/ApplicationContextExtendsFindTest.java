6b651ec8bde1741de5c960475e98a47256a417a5
choihongjun <vvclsbalam@gmail.com>
2025-03-10 03:29:20 +0900

    feat: 선택 항목 중 새 스크래치 파일 생성 테스트 구현

--- src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -54,6 +54,16 @@ public class ApplicationContextExtendsFindTest {
         }
     }
 
+    @Test
+    @DisplayName("부모 타입으로 모두 조회하기 - Object")
+    void findAllBeanByObjectType() {
+        Map<String, Object> beansOfType = ac.getBeansOfType(Object.class);
+        for (String key : beansOfType.keySet()) {
+            System.out.println("key = " + key + " value=" +
+                    beansOfType.get(key));
+        }
+    }
+
     @Configuration
     static class TestConfig {
         @Bean
