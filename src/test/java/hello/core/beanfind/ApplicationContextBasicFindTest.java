134f488fe3fd5da5967b3dc9f2ad8bad24c12eb8
choihongjun <vvclsbalam@gmail.com>
2025-03-10 02:45:54 +0900

    feat: 테스트 패키지로 이동

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -3,18 +3,18 @@ package hello.core.beanfind;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextBasicFindTest {
 
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
     @Test
     @DisplayName("빈 이름으로 조회")
     void findBeanByName() {
