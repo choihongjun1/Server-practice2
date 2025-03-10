6b3c9a6d5dd52df0fc054719e0a24f3168c67be5
choihongjun <vvclsbalam@gmail.com>
2025-03-10 14:29:22 +0900

    feat: memberRepository 빈 호출 로그 출력

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -23,6 +23,7 @@ public class AppConfig {
 
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
