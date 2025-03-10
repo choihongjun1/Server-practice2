8d3c01abd0fd48fac5e52ed291d261b0612064c9
choihongjun <vvclsbalam@gmail.com>
2025-03-10 14:28:19 +0900

    feat: OrderService 빈 호출 로그 출력

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -28,6 +28,7 @@ public class AppConfig {
 
     @Bean
     public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(),discountPolicy());
     }
 
