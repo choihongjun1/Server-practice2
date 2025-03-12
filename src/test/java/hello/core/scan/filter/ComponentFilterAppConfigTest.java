66453e0a5ee81d9b93787f95e7a1bafbba5f5b9f
choihongjun <vvclsbalam@gmail.com>
2025-03-13 01:51:16 +0900

    feat: ComponentFilterAppConfig 임시 컨테이너 구현

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,16 @@
+package hello.core.scan.filter;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+public class ComponentFilterAppConfigTest {
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyIncludeComponent.class),
+            excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = MyExcludeComponent.class)
+    )
+    static class ComponentFilterAppConfig {
+    }
+}
