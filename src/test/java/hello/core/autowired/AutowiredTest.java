b7f9711e4555390ce21ba0313ad51d78730736de
choihongjun <vvclsbalam@gmail.com>
2025-03-17 00:48:52 +0900

    feat: TestBean 임시 컨테이너 setBean2 메소드 구현

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.lang.Nullable;
 
 public class AutowiredTest {
 
@@ -18,6 +19,12 @@ public class AutowiredTest {
         @Autowired(required = false)
         public void setNoBean1(Member member) {
             System.out.println("setNoBean1 = " + member);
+        }
+
+        //null 호출
+        @Autowired
+        public void setNoBean2(@Nullable Member member) {
+            System.out.println("setNoBean2 = " + member);
         }
     }
 }
