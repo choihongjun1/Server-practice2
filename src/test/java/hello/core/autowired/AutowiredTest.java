202bab3acaad543fc2f5f622b55524f4f26ab2d0
choihongjun <vvclsbalam@gmail.com>
2025-03-17 00:50:29 +0900

    feat: TestBean 임시 컨테이너 setBean3 메소드 구현

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -7,6 +7,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.lang.Nullable;
 
+import java.util.Optional;
+
 public class AutowiredTest {
 
     @Test
@@ -25,6 +27,12 @@ public class AutowiredTest {
         @Autowired
         public void setNoBean2(@Nullable Member member) {
             System.out.println("setNoBean2 = " + member);
+        }
+
+        //Optional.empty 호출
+        @Autowired(required = false)
+        public void setNoBean3(Optional<Member> member) {
+            System.out.println("setNoBean3 = " + member);
         }
     }
 }
