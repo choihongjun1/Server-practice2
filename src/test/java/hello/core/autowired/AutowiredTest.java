902c31d86691f1a61203a4f22723b121b30aa73d
choihongjun <vvclsbalam@gmail.com>
2025-03-17 00:47:07 +0900

    feat: TestBean 임시 컨테이너 setBean1 메소드 구현

--- src/test/java/hello/core/autowired/AutowiredTest.java
+++ src/test/java/hello/core/autowired/AutowiredTest.java
@@ -1,13 +1,23 @@
 package hello.core.autowired;
 
+import hello.core.member.Member;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class AutowiredTest {
 
     @Test
-    public void autowiredOption() {
+    void autowiredOption() {
         ApplicationContext ac = new AnnotationConfigApplicationContext(TestBean.class);
+    }
+
+    static class TestBean {
+        //호출 안됨
+        @Autowired(required = false)
+        public void setNoBean1(Member member) {
+            System.out.println("setNoBean1 = " + member);
+        }
     }
 }
