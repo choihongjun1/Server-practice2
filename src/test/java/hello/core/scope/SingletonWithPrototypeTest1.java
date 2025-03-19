82dd74bfefc6c441f4af647308cdf473146476a4
choihongjun <vvclsbalam@gmail.com>
2025-03-20 02:06:28 +0900

    feat: 프로토타입 스코프 생성

---
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -0,0 +1,31 @@
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.context.annotation.Scope;
+
+public class SingletonWithPrototypeTest1 {
+
+    @Scope("prototype")
+    static class PrototypeBean {
+        private int count = 0;
+
+        public void addCount() {
+            count++;
+        }
+
+        public int getCount() {
+            return count;
+        }
+
+        @PostConstruct
+        public void init() {
+            System.out.println("PrototypeBean.init " + this);
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("PrototypeBean.destroy");
+        }
+    }
+}
