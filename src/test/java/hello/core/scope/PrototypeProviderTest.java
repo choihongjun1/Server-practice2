0b22eaddc4e57a35459979e48bc1496e991b4de5
choihongjun <vvclsbalam@gmail.com>
2025-03-21 13:11:13 +0900

    feat: ClientBean 클래스 구현

--- src/test/java/hello/core/scope/PrototypeProviderTest.java
+++ src/test/java/hello/core/scope/PrototypeProviderTest.java
@@ -1,6 +1,8 @@
 package hello.core.scope;
 
+import org.junit.jupiter.api.AutoClose;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -9,7 +11,7 @@ public class PrototypeProviderTest {
 
     @Test
     void providerTest() {
-        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonWithPrototypeTest1.ClientBean.class, PrototypeTest.PrototypeBean.class);
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(ClientBean.class, PrototypeBean.class);
 
         SingletonWithPrototypeTest1.ClientBean clientBean1 = ac.getBean(SingletonWithPrototypeTest1.ClientBean.class);
         int count1 = clientBean1.logic();
@@ -18,5 +20,16 @@ public class PrototypeProviderTest {
         SingletonWithPrototypeTest1.ClientBean clientBean2 = ac.getBean(SingletonWithPrototypeTest1.ClientBean.class);
         int count2 = clientBean2.logic();
         assertThat(count2).isEqualTo(1);
+    }
+
+    static class ClientBean {
+        @AutoClose
+        private ApplicationContext ac;
+        public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
+            prototypeBean.addCount();
+            int count = prototypeBean.getCount();
+            return count;
+        }
     }
 }
